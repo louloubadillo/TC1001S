e286212ff1b1afdc613b9fec32ccf2a47355231d
Martha del Rio <bluedelrio@gmail.com>
2022-05-12 11:12:26 -0500

    Fixed doc

--- Games/memory.py
+++ Games/memory.py
@@ -1,12 +1,9 @@
 """Memory, puzzle game of number pairs.
-
 Team
 Martha del Río
 Edgar Rostro
 Lourdes Badillo
-
 Exercises:
-
 1. Count and print how many taps occur.
 2. Decrease the number of tiles to a 4x4 grid.
 3. Detect when all tiles are revealed.
@@ -19,10 +16,12 @@ from turtle import *
 
 from freegames import path
 
+#decreased the number of total tiles, from 64 to 16
 car = path('car.gif')
-tiles = list(range(32)) * 2
+tiles = list(range(8)) * 2
+#add score and tap states
 state = {'mark': None, 'score': 0, 'tap': 0}
-hide = [True] * 64
+hide = [True] * 16
 counter = 0
 
 
@@ -38,21 +37,22 @@ def square(x, y):
         left(90)
     end_fill()
 
-
+#centered board (4x4)
 def index(x, y):
     """Convert (x, y) coordinates to tiles index."""
-    return int((x + 200) // 50 + ((y + 200) // 50) * 8)
+    return int((x + 100) // 50 + ((y + 100) // 50) * 4)
 
 
 def xy(count):
     """Convert tiles count to (x, y) coordinates."""
-    return (count % 8) * 50 - 200, (count // 8) * 50 - 200
+    return (count % 4) * 50 - 100, (count // 4) * 50 - 100
 
 
 def tap(x, y):
     """Update mark and hidden tiles based on tap."""
     spot = index(x, y)
     mark = state['mark']
+    #increment taps
     state['tap'] += 1
 
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
@@ -61,6 +61,7 @@ def tap(x, y):
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
+        #increment score
         state['score'] += 1
 
 
@@ -71,7 +72,7 @@ def draw():
     shape(car)
     stamp()
 
-    for count in range(64):
+    for count in range(16):
         if hide[count]:
             x, y = xy(count)
             square(x, y)
@@ -88,8 +89,7 @@ def draw():
 
     update()
     ontimer(draw, 100)
-
-
+    
 shuffle(tiles)
 setup(420, 420, 370, 0)
 addshape(car)
@@ -98,7 +98,11 @@ tracer(False)
 onscreenclick(tap)
 draw()
 done()
+#print final score and total taps
 print("Total Taps: ")
 print(state["tap"])
 print("Final Score: ")
 print(state["score"])
+#prints a winning banner if the game is completed
+if state["score"] == 8:
+    print("Ganaste, ¡wuuu!")
